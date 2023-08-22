e450ad5b82c2c642293596106ad26a30d482444f
Eran Gross <eran@gross.support>
2023-08-22 11:14:27 +0300

    deleting old files

--- disable_alarm_action.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-# Create CloudWatch client
-cloudwatch = boto3.client('cloudwatch')
-
-# Disable alarm
-cloudwatch.disable_alarm_actions(
-  AlarmNames=['Web_Server_CPU_Utilization'],
-)
